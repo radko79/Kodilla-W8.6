9e11ce70a37f842fbef5b62b7f83e27254dffbc1
Radoslaw Podgorski <radoslaw.podgorski@gmail.com>
2018-01-24 17:20:38 +0000

    Add changed to js

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,11 @@
 		alert('The result is: ' + value);
 		console.log('The result of the equation a^2 - 2ab + b^2, where a = ' + a + ' and b = ' + b + ' is: ' + value);
 		if (value == 0) {
-			console.log('Bull\'s eye! The result is 0.');
+			console.log('Bull\'s eye! The result is equal to 0.');
+		} else if (value < 0) {
+			console.log('The result is less then 0.');
 		} else {
-			console.log('Not this time. The result is different than 0.');
+			console.log('The result is greater then 0.');
 		}
 	//document.getElementById("result").innerHTML = triangleArea;
 	
